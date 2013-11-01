db1c64c87ab242f9bc1d459f7b58794038321e5a
Theodore Cleveland <tcclevela@gmail.com>
2013-11-01 11:09:05 -0400

    initial exercises and empty stylesheet'

---
+++ main.js
@@ -0,0 +1,7 @@
+// add an event listener that prompts for user information when button is clicked
+
+myButton = document.getElementById('add-person-button')
+
+// addEventListener
+// onclick
+// removeEventListener

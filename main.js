e048ecfa3bf27cd450eebffc5977ce548cd7c683
Theodore Cleveland <tcclevela@gmail.com>
2013-11-01 15:50:25 -0400

    finishes in class lesson

--- main.js
+++ main.js
@@ -10,22 +10,35 @@ var Page = {
 
 
 	setButtonEvent: function() {
+		Page.myButton.addEventListener('click', Page.clickResponse);
 		// write a function that adds an event listener to the button
 		// that will call the promptInfo function.
 		// after the button is clicked, it should say "change person info"
 	},
 
 	setLinkEvent: function() {
+		// we're defining a 'callback,' which is a function defined in-line
+		Page.myLink.addEventListener('click', function(event){
+			event.preventDefault();
+			Page.clickResponse();
+
+		});
 		// write a function that will add an event listener to the link,
 		// making sure that you account for the link's natural redirct.
 	},
 
+	clickResponse: function() {
+		Page.promptInfo();
+		Page.changeButtonText();	
+		Page.changeLinkText();
+		Page.myButton.removeEventListener('click', Page.clickResponse);
+	},
 
 	promptInfo: function() {
 		var name = prompt("what is your name?"),
 			age = prompt('how old are you?'),
 			email = prompt('what is your email');
-			addPerson(name, age, email);
+			Page.addPerson(name, age, email);
 	},
 
 	addPerson: function(name, age, email) {
@@ -35,6 +48,14 @@ var Page = {
 			emailField = "<h2>" + email + "</h2>";
 
 		userDiv.innerHTML = nameHeader + ageField	+ emailField;
+	},
+
+	changeButtonText: function() {
+		Page.myButton.innerHTML = 'Change Person Info';
+	},
+
+	changeLinkText: function() {
+		Page.myLink.innerHTML = 'Change Person Info';
 	}
 };
 
